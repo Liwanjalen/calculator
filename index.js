504c6b2330f3f19d797053454ee44601330598de
Liwanjalen <liwanjalenzki@gmail.com>
2023-07-14 20:51:16 +0800

    operation function done

--- index.js
+++ index.js
@@ -2,3 +2,36 @@
 console.log("Hello World from index.js!");
 //it is working
 // lets create a calculator app with index.html and index.js
+// the calculator will have 4 functions: add, subtract, multiply, divide
+
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const operator = document.getElementById("operator");
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+function operate(num1, operator, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
